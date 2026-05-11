a00bc07b7877c36e2a03888771c499228440479c
Eber Johns C D <eberjohnspro@gmail.com>
2026-05-11 15:07:22 +0530

    version 2 of the app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.example.geoalarm" // (Keep whatever yours is)
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
 
         // --- THE VAULT READER ---
         val keystoreProperties = Properties()
